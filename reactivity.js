e31b8a3bf3bb9e3fb8775909960259f1868d3188
= <=>
2021-10-03 15:44:23 +0500

    simple reactivity

---
+++ reactivity.js
@@ -0,0 +1,29 @@
+let price = 5;
+let quantity = 2;
+let total = 0;
+
+let dep = new Set(); // To store our effects
+
+let effect = function () {
+  total = price * quantity;
+}
+
+function track() {
+  dep.add(effect); // Adding the code in deps
+}
+
+function trigger() {
+  dep.forEach(effect => {  // Trigger the code again when any value changes
+    effect();
+  });
+}
+track();
+trigger();
+
+console.log(`total is ${total}`);
+
+price = 20;
+
+trigger();
+
+console.log(`total is ${total}`);
